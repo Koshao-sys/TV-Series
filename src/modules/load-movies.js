3dd2c93fae9645d88c95e25a6060365e01186e9d
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-03-15 19:54:16 +0300

    Resolve conflicts in load-api.js

--- src/modules/load-movies.js
+++ src/modules/load-movies.js
@@ -1,18 +1,19 @@
-import { getMovies, getLikes } from './api.js';
+import { getMovies, getLikes, postLikes } from './api.js';
 import popup from './commentPopup.js';
 
+const object = [];
 const commentPopup = document.querySelector('.comment-popup');
 
-const buildDivElement = (ldMovies, resultOb, cnt) => {
+const buildDivElement = (ldMovies, resultOb) => {
   const moviesItem = document.createElement('div');
   moviesItem.classList = 'moviesItem';
   moviesItem.innerHTML = `
-      <img src="${resultOb.image.medium}" alt="">
+      <img class="image" src="${resultOb.image.medium}" alt="">
       <div class="likes-name">
-          <p>${resultOb.name}</p>
-          <i class="lni lni-heart"></i>
+          <p class="title">${resultOb.name}</p>
+          <i class="lni lni-heart" id="${resultOb.id}"></i>
       </div>
-      <p class="likes-count">${cnt} Likes</p>`;
+      <p class="likes-count">${resultOb.likes} Likes</p>`;
   const btn = document.createElement('button');
   btn.classList = 'comment-btn';
   btn.innerHTML = 'Comments';
@@ -27,11 +28,19 @@ const buildDivElement = (ldMovies, resultOb, cnt) => {
 const CreateMovieElement = (lodMovies, resultObj) => {
   const flix = document.querySelector('.flix');
   lodMovies.innerHTML = '';
-  getLikes().then(() => {
-    for (let i = 0; i < resultObj.length; i += 1) {
-      buildDivElement(lodMovies, resultObj[i], 0);
+  getLikes().then((result) => {
+    const likesData = result.data;
+    const updatedMovies = resultObj.map((movie) => {
+      const like = likesData.find((like) => like.item_id === movie.id);
+      // eslint-disable-next-line no-unused-expressions
+      like ? movie.likes = like.likes : movie.likes = 0;
+      object.push(movie);
+      return movie;
+    });
+    for (let i = 0; i < updatedMovies.length; i += 1) {
+      buildDivElement(lodMovies, updatedMovies[i]);
     }
-    flix.textContent = `Flix(${resultObj.length})`;
+    flix.textContent = `Flix (${resultObj.length})`;
   });
 };
 
@@ -43,4 +52,26 @@ const showMovies = () => {
     }
   });
 };
+
+const parentElement = document.querySelector('.movie-container');
+parentElement.addEventListener('click', (e) => {
+  if (e.target.matches('.lni.lni-heart')) {
+    const flixId = e.target.id;
+    postLikes(flixId).then((result) => {
+      if (result.success) {
+        getLikes().then((resultLike) => {
+          const newLikes = resultLike.data.find((newlikes) => newlikes.item_id === flixId);
+          const newParent = e.target.parentElement.parentElement;
+          const addLikes = newParent.querySelector('.likes-count');
+          addLikes.classList.add('transition');
+          addLikes.textContent = `${newLikes.likes} Likes`;
+          setTimeout(() => {
+            addLikes.classList.remove('transition');
+          }, 500);
+        });
+      }
+    });
+  }
+});
+
 export default showMovies;
